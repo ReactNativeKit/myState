69f4a79bd1d0ad847e99132b3eb7ea5bd3a3a364
Abhinav Anand <abhi9vanand@gmail.com>
2022-04-05 13:48:19 +0530

    files added

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [name, setName] = useState('Abhinav');
+  const [person, setPerson] = useState({ name: 'Tushar', age: 22 });
+
+  const clickHandler = () => {
+    setName('Prem');
+    setPerson({ name: 'Edward', age: 24 });
+  };
+
+  return (
+      <View style={styles.container}>
+      <Text>My name is {name}.</Text>
+      <Text>His name is {person.name} and his age is {person.age}.{'\n'}</Text>
+      <View>
+        <Button color="#92BA92" title='update state' onPress={clickHandler} />  
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1DDBF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
